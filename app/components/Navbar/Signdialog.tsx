6cb04ae28ad8c9547ed0c236f7b4db15644d5217
HienMinh56 <dlhminh0506@gmail.com>
2023-09-20 10:45:26 +0700

    update Register form

--- app/components/Navbar/Signdialog.tsx
+++ app/components/Navbar/Signdialog.tsx
@@ -2,7 +2,6 @@ import { Dialog, Transition } from '@headlessui/react'
 import { Fragment, useState } from 'react'
 import { LockClosedIcon } from '@heroicons/react/20/solid'
 
-
 const Signin = () => {
     let [isOpen, setIsOpen] = useState(false)
 
@@ -66,7 +65,7 @@ const Signin = () => {
                                             <form className="mt-8 space-y-6" action="#" method="POST">
                                                 <input type="hidden" name="remember" defaultValue="true" />
                                                 <div className="-space-y-px rounded-md shadow-sm">
-                                                    <div>
+                                                    <div style={{padding: '5px'}}>
                                                         <label htmlFor="email-address" className="sr-only">
                                                             Email address
                                                         </label>
@@ -80,7 +79,7 @@ const Signin = () => {
                                                             placeholder="Email address"
                                                         />
                                                     </div>
-                                                    <div>
+                                                    <div style={{padding: '5px'}}>
                                                         <label htmlFor="password" className="sr-only">
                                                             Password
                                                         </label>
