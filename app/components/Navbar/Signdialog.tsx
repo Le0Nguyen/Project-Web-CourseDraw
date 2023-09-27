bcbc71f7c04f38d4d85fcdb41f18e55f3db4dba8
HienMinh56 <dlhminh0506@gmail.com>
2023-09-27 14:29:49 +0700

    token test

--- app/components/Navbar/Signdialog.tsx
+++ app/components/Navbar/Signdialog.tsx
@@ -1,9 +1,52 @@
 import { Dialog, Transition } from "@headlessui/react";
-import { Fragment, useState } from "react";
+import { Fragment, useState, useEffect } from "react";
 import { LockClosedIcon } from "@heroicons/react/20/solid";
 import Link from "next/link";
+import axios from "axios"
 
 const Signin = () => {
+  // useEffect(() => {
+  //   // Initialize the users array with an empty array.
+  //   setUsers([]);
+  // }, []);
+
+  //   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+
+  //     const payload = {
+  //       email: event.currentTarget.email.value,
+  //       passWord: event.currentTarget.password.value,
+  //     };
+  // }; 
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    const data = JSON.stringify({
+      username,
+      password
+    });
+
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'http://localhost:8080/login',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: data
+    };
+
+    axios.request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   let [isOpen, setIsOpen] = useState(false);
 
   const closeModal = () => {
@@ -13,6 +56,29 @@ const Signin = () => {
   const openModal = () => {
     setIsOpen(true);
   };
+
+  // const [users, setUsers] = useState([]);
+
+  // const fetchUsers = async () => {
+  //   const response = await axios.get("http://localhost:8080/users");
+  //   setUsers(response.data);
+  // };
+
+  // const handleLogin = async () => {
+  //   const response = await axios.post("http://localhost:8080/login", {
+  //       username: emailAddress,
+  //       password: passWord,
+  //   });
+
+  //   // Nếu đăng nhập thành công, lưu thông tin người dùng vào localStorage.
+  //   if (response.status === 200) {
+  //       localStorage.setItem("user", JSON.stringify(response.data));
+  //       window.location.href = "/";
+  //   } else {
+  //       // Hiển thị thông báo lỗi cho người dùng.
+  //       alert("Login failed.");
+  //   }
+  // };
 
   return (
     <>
@@ -74,17 +140,19 @@ const Signin = () => {
                         />
                         <div className="-space-y-px rounded-md shadow-sm">
                           <div style={{ padding: "5px" }}>
-                            <label htmlFor="email-address" className="sr-only">
-                              Email address
+                            <label htmlFor="user-name" className="sr-only">
+                              Username
                             </label>
                             <input
-                              id="email-address"
-                              name="email"
-                              type="email"
-                              autoComplete="email"
+                              value={username}
+                              onChange={(val) => setUsername(val.target.value)}
+                              id="user-name"
+                              name="username"
+                              type="username"
+                              autoComplete="username"
                               required
                               className="relative block w-full appearance-none rounded-none rounded-t-md border border-grey500 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                              placeholder="Email address"
+                              placeholder="username"
                             />
                           </div>
                           <div style={{ padding: "5px" }}>
@@ -92,6 +160,8 @@ const Signin = () => {
                               Password
                             </label>
                             <input
+                             value={password}
+                             onChange={(val) => setPassword(val.target.value)}
                               id="password"
                               name="password"
                               type="password"
@@ -129,6 +199,7 @@ const Signin = () => {
                         </div>
                         <div>
                           <button
+                            onClick={handleLogin}
                             type="submit"
                             className="group relative flex w-full justify-center rounded-md border border-transparent bg-purple py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                           >
