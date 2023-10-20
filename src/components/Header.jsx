f06e3a71eda6b2f7b89427aec117e7d763c602bc
Fancy <133141329+HienMinh56@users.noreply.github.com>
2023-10-20 15:17:14 +0700

    add checkout (#12)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,6 @@ const Header = () => {
     <div className='flex justify-between px-4 pt-4'>
         <h2>Dashboard</h2>
         <h2>Welcome Back, Clint</h2>
-        <h2>Welcome Back, Clint</h2>
-
     </div>
   )
 }
